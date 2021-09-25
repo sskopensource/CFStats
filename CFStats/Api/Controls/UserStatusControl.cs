22ca8dc7aea28d298f547e1d1ba416bbf9a438e1
Suveen Kumar <suveenkumar1997@gmail.com>
2021-09-25 21:55:21 +0530

    Initial version of files

---
+++ CFStats/Api/Controls/UserStatusControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api
+{
+    public class UserStatusControl
+    {
+        public static async Task<UserStatusModel> LoadUserStatus(string handle)
+        {
+            string url = "https://codeforces.com/api/user.status?handle=";
+            url += handle;
+            Console.WriteLine(handle);
+
+            using (HttpResponseMessage response = await ApiHelper.ApiClient.GetAsync(url))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    UserStatusModel userModel = await response.Content.ReadAsAsync<UserStatusModel>();
+                    return userModel;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
